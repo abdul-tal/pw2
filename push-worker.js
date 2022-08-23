3e85d1b77ea156f25cc6aae2cbb577eb9714a9f8
Abdul Rahman <abdul.rahman@autodesk.com>
2022-08-23 20:40:13 +0530

    airship setup

---
+++ push-worker.js
@@ -0,0 +1,11 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1661266792.7283144
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  
+  defaultIcon: 'https://picsum.photos/200/300',
+  defaultTitle: 'Test Push\u002D1',
+  defaultActionURL: 'https://reliable\u002Dgaufre\u002D8eb389.netlify.app/',
+  appKey: '2ieSnJLsT3GZgrNuILEqpQ',
+  token: 'MToyaWVTbkpMc1QzR1pnck51SUxFcXBROm9aenhPTEtfcWdTUEhzenFwa3B2OXpyOE0xY201blpZNTd6d0g0UzhuVjQ',
+  vapidPublicKey: 'BEhAhDAiRdJSxWzy0AVRuu17DiBTf37mr7g3TBcpzPTbb8lDWnoumKr9wXsecCn-YUh7l5SdrfuOk_Pen4moggc='
+})
